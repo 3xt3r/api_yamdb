c1bdb6edfd048414c28b5d1793cd046d9352a79e
Анастасия Кольцова <kolcova1910@mail.ru>
2023-01-05 03:39:46 +0300

    Добавление вьюхи

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from reviews.models import Comment, Review
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -17,4 +17,18 @@ class AdminOrReadOnly(permissions.BasePermission):
             return True
         return request.user.is_authenticated and request.user.is_admin
 
-#class CommentPermissions(permissions.BasePermission)
+
+class ReviewCommentPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated and request.user.is_admin:
+            return True
+        if ((type(obj) == Comment or type(obj) == Review)
+            and request.user.is_authenticated
+                and request.user.is_moderator):
+            return True
+        return (obj.author == request.user
+                or request.method in permissions.SAFE_METHODS)
